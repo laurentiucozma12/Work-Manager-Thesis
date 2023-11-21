c19875d45b97149a23a8587ef421e74c07f17ab8
Lau <laurentiucozma12@gmail.com>
2023-11-21 20:57:00 +0200

    some bugs resolved

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,7 @@
 <?php
-define('WEB_PATH', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']);
+$requestScheme = isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : 'http';
+
+define('WEB_PATH', $requestScheme.'://'.$_SERVER['HTTP_HOST']);
 define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
 session_start();
 ?>
